1b37b3d156edb394e231e32724b34837796abcae
root <Abiodun Adeniji>
2023-06-27 16:14:00 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,81 +1,66 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
- * @format: format string.
- * Return: number of characters printed.
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-    int i, printed_chars = 0;
-    va_list list;
-    
-    if (format == NULL)
-        return (-1);
-    
-    va_start(list, format);
-    
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            putchar(format[i]);
-            printed_chars++;
-        }
-        else if (format[i + 1] == '%')
-        {
-            putchar('%');
-            printed_chars++;
-            i++;
-        }
-        else if (format[i + 1] == 'c')
-        {
-            int c = va_arg(list, int);
-            putchar(c);
-            printed_chars++;
-            i++;
-        }
-        else if (format[i + 1] == 's')
-        {
-            char *s = va_arg(list, char *);
-            printed_chars += _puts(s);
-            i++;
-        }
-    }
-    
-    va_end(list);
-    
-    return (printed_chars);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
 /**
- * _puts - Prints a string.
- * @str: The string to be printed.
- * Return: Number of characters printed.
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-int _puts(char *str)
+void print_buffer(char buffer[], int *buff_ind)
 {
-    int i, count = 0;
-    
-    if (str == NULL)
-        str = "(null)";
-    
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        putchar(str[i]);
-        count++;
-    }
-    
-    return (count);
-}
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-/**
- * putchar - Writes a character to stdout.
- * @c: The character to be written.
- * Return: On success, the character written is returned.
- *         On error, -1 is returned.
- */
-int putchar(int c)
-{
-    return (write(1, &c, 1));
+	*buff_ind = 0;
 }
